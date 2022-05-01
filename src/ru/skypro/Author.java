804bf0a235c3e4364d45ee2d020b097285b21f63
Chirkov Maxim <maxim.chirkov@skyeng.ru>
2022-05-01 15:06:22 +0700

    first-try

---
+++ src/ru/skypro/Author.java
@@ -0,0 +1,24 @@
+package ru.skypro;
+
+public class Author {
+    public String authorName;
+    public String authorSurname;
+
+
+    public Author(String authorName, String authorSurname) {
+        this.authorName = authorName;
+        this.authorSurname = authorSurname;
+    }
+
+    public String getAuthorFullName() {
+        return authorName + " " + authorSurname;
+    }
+
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public String getAuthorSurname() {
+        return authorSurname;
+    }
+}
